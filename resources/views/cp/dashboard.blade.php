c7ea903e828946658235840df48b654ca5d4ee07
Jack Whiting <hi@jackwhiting.co.uk>
2021-03-03 16:12:49 +0000

    adding fallback messages for if you haven't set things up

--- resources/views/cp/dashboard.blade.php
+++ resources/views/cp/dashboard.blade.php
@@ -4,20 +4,37 @@
     <div class="flex items-center justify-between mb-3">
         <h1>Shopify</h1>
 
-        <a href="{{ $shopify_url }}" class="btn-primary" target="_blank">Go To Shopify Admin</a>
+        @if ($shopify_url)
+            <a href="{{ $shopify_url }}" class="btn-primary" target="_blank">Go To Shopify Admin</a>
+        @endif
     </div>
 
-    <div class="card">
-        <h2 class="mb-2">Import Products</h2>
-        <p class="mb-2 text-sm max-w-md leading-loose">This will fetch all of your product data from Shopify. Please note if you have `overwrite_content` set to true, this will overwrite any updates made in the dashboard.</p>
+    @if ($can_run_import)
+        <div class="card">
+            <h2 class="mb-2">Import Products</h2>
+            <p class="mb-2 text-sm max-w-md leading-loose">This will fetch all of your product data from Shopify. Please note if you have any `overwrite` option set to true in the config product data will be replaced.</p>
 
-        <import-products-button url="{{ cp_route('shopify.products.fetchAll') }}"></import-products-button>
-    </div>
+            <import-products-button url="{{ cp_route('shopify.products.fetchAll') }}"></import-products-button>
+        </div>
 
-    <div class="card mt-4">
-        <h2 class="mb-2">Import Single Product</h2>
-        <p class="mb-2 text-sm max-w-md leading-loose">Fetch a single product's data from Shopify. Please note if you have `overwrite_content` set to true, this will overwrite any updates made in the dashboard.</p>
+        <div class="card mt-4">
+            <h2 class="mb-2">Import Single Product</h2>
+            <p class="mb-2 text-sm max-w-md leading-loose">Fetch a single product's data from Shopify. Please note if you have any `overwrite` option set to true in the config product data will be replaced.</p>
 
-        <import-product-button url="{{ cp_route('shopify.products.fetch') }}"></import-product-button>
-    </div>
+            <import-product-button url="{{ cp_route('shopify.products.fetch') }}"></import-product-button>
+        </div>
+    @else
+        <div class="card">
+            <h2 class="mb-2">Set Things Up</h2>
+
+            <p class="mb-2 text-sm max-w-md leading-loose">Looks like you haven't set everything up yet. You might be missing one of the following:</p>
+
+            <ul class="mb-2 pl-3 list-decimal text-sm max-w-md leading-loose">
+                <li>Have you updated your `env` variables?</li>
+                <li>Have you published the assets?</li>
+            </ul>
+
+            <p class="text-sm max-w-md leading-loose">If not, ensure you've ran through the <a href="#" class="text-blue-700">Quickstart</a> guide on the documentation.</p>
+        </div>
+    @endif
 @endsection
--- src/Http/Controllers/CP/DashboardController.php
+++ src/Http/Controllers/CP/DashboardController.php
@@ -8,10 +8,12 @@ class DashboardController extends CpController
 {
     public function index()
     {
-        $shopify_url = 'https://' . config('shopify.app_url') . '/admin';
+        $shopify_url = (config('shopify.url')) ? 'https://' . config('shopify.url') . '/admin' : null;
+        $can_run_import = (config('shopify.url') && config('shopify.auth_key') && config('shopify.auth_password'));
 
         return view('shopify::cp.dashboard', [
-            'shopify_url' => $shopify_url
+            'shopify_url' => $shopify_url,
+            'can_run_import' => $can_run_import
         ]);
     }
 }
