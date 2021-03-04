8a0d4ebabf7b4026e974268004e0503e3b453af8
Jack Whiting <hi@jackwhiting.co.uk>
2021-03-04 02:52:05 +0000

    Adding webhook to detect when products are deleted

--- routes/actions.php
+++ routes/actions.php
@@ -5,6 +5,9 @@ use App\Http\Middleware\VerifyCsrfToken;
 Route::post('/webhooks/order', 'Webhooks\OrderCreationController@listen')
     ->withoutMiddleware([VerifyCsrfToken::class]);
 
+Route::post('/webhooks/product-deletion', 'Webhooks\ProductDeletionController@listen')
+    ->withoutMiddleware([VerifyCsrfToken::class]);
+
 Route::get('/variants/{product}', 'Actions\VariantsController@fetch')
     ->name('shopify.variants.fetch');
 
