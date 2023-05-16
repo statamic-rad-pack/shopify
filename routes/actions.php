5e3eda47954cb3be1c65f24418438f11956a9682
Andrew Topping <andrew@thoughtcollective.com>
2023-05-16 16:01:26 +0100

    Update Controller Handling
    Update to handle how new versions of Laravel deal with controllers.

--- routes/actions.php
+++ routes/actions.php
@@ -1,22 +1,25 @@
 <?php
 
 use App\Http\Middleware\VerifyCsrfToken;
+use Jackabox\Shopify\Http\Controllers\Webhooks\OrderCreateController;
+use Jackabox\Shopify\Http\Controllers\Webhooks\ProductCreateUpdateController;
+use Jackabox\Shopify\Http\Controllers\Webhooks\ProductDeleteController;
 use Illuminate\Support\Facades\Route;
 
 Route::name('shopify.')->group(function () {
-    Route::post('/webhook/order', 'Webhooks\OrderCreateController')
+    Route::post('/webhook/order', OrderCreateController::class)
         ->withoutMiddleware([VerifyCsrfToken::class])
         ->name('webhook.order.created');
 
-    Route::post('/webhook/product/create', 'Webhooks\ProductCreateUpdateController')
+    Route::post('/webhook/product/create', ProductCreateUpdateController::class)
         ->withoutMiddleware([VerifyCsrfToken::class])
         ->name('webhook.product.create');
 
-    Route::post('/webhook/product/update', 'Webhooks\ProductCreateUpdateController')
+    Route::post('/webhook/product/update', ProductCreateUpdateController::class)
         ->withoutMiddleware([VerifyCsrfToken::class])
         ->name('webhook.product.update');
 
-    Route::post('/webhook/product/delete', 'Webhooks\ProductDeleteController')
+    Route::post('/webhook/product/delete', ProductDeleteController::class)
         ->withoutMiddleware([VerifyCsrfToken::class])
         ->name('webhook.product.delete');
 
