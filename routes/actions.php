f6dcbec6fca18b6ed10e4a383ab740cfec16ab6e
Ryan Mitchell <ryan@thoughtcollective.com>
2023-09-04 18:38:40 +0100

    Use Laravel 10 conventions for routes
    Co-Authored-By: Andrew Topping <andrew@thoughtcollective.com>

--- routes/actions.php
+++ routes/actions.php
@@ -2,25 +2,29 @@
 
 use App\Http\Middleware\VerifyCsrfToken;
 use Illuminate\Support\Facades\Route;
+use StatamicRadPack\Shopify\Http\Controllers\Actions\VariantsController;
+use StatamicRadPack\Shopify\Http\Controllers\Webhooks\OrderCreateController;
+use StatamicRadPack\Shopify\Http\Controllers\Webhooks\ProductCreateUpdateController;
+use StatamicRadPack\Shopify\Http\Controllers\Webhooks\ProductDeleteController;
 
 Route::name('shopify.')->group(function () {
-    Route::post('/webhook/order', 'Webhooks\OrderCreateController')
+    Route::post('/webhook/order', OrderCreateController::class)
         ->withoutMiddleware([VerifyCsrfToken::class])
         ->name('webhook.order.created');
 
-    Route::post('/webhook/product/create', 'Webhooks\ProductCreateUpdateController')
+    Route::post('/webhook/product/create', ProductCreateUpdateController::class)
         ->withoutMiddleware([VerifyCsrfToken::class])
         ->name('webhook.product.create');
 
-    Route::post('/webhook/product/update', 'Webhooks\ProductCreateUpdateController')
+    Route::post('/webhook/product/update', ProductCreateUpdateController::class)
         ->withoutMiddleware([VerifyCsrfToken::class])
         ->name('webhook.product.update');
 
-    Route::post('/webhook/product/delete', 'Webhooks\ProductDeleteController')
+    Route::post('/webhook/product/delete', ProductDeleteController::class)
         ->withoutMiddleware([VerifyCsrfToken::class])
         ->name('webhook.product.delete');
 
-    Route::get('/variants/{product}', 'Actions\VariantsController@fetch')
+    Route::get('/variants/{product}', [VariantsController::class, 'fetch'])
         ->name('variants.fetch');
 });
 
--- routes/cp.php
+++ routes/cp.php
@@ -1,29 +1,36 @@
 <?php
 
-Route::get('/shopify', 'CP\DashboardController@index')
+use StatamicRadPack\Shopify\Http\Controllers\Actions\VariantsController;
+use StatamicRadPack\Shopify\Http\Controllers\CP\DashboardController;
+use StatamicRadPack\Shopify\Http\Controllers\CP\ImportCollectionsController;
+use StatamicRadPack\Shopify\Http\Controllers\CP\ImportProductsController;
+use StatamicRadPack\Shopify\Http\Controllers\CP\ProductsController;
+use StatamicRadPack\Shopify\Http\Controllers\CP\VariantsController;
+
+Route::get('/shopify',[DashboardController::class, 'index'])
     ->name('shopify.index');
 
 // Dashboard Imports
-Route::get('/shopify/import/products/all', 'CP\ImportProductsController@fetchAll')
+Route::get('/shopify/import/collections/all', [ImportCollectionsController::class, 'fetchAll'])
+    ->name('shopify.collections.fetchAll');
+    
+Route::get('/shopify/import/products/all', [ImportProductsController::class, 'fetchAll'])
     ->name('shopify.products.fetchAll');
 
-Route::get('/shopify/import/products/single', 'CP\ImportProductsController@fetchSingleProduct')
+Route::get('/shopify/import/products/single', [ImportProductsController::class, 'fetchSingleProduct'])
     ->name('shopify.products.fetch');
 
-Route::get('/shopify/import/collections/all', 'CP\ImportCollectionsController@fetchAll')
-    ->name('shopify.collections.fetchAll');
-
 // Others
-Route::get('/shopify/products', 'CP\ProductsController@index')
+Route::get('/shopify/products', [ProductsController::class, 'index'])
     ->name('shopify.products');
 
-Route::get('/shopify/variants/{product}', 'CP\VariantsController@fetch')
+Route::get('/shopify/variants/{product}', [VariantsController::class, 'fetch'])
     ->name('shopify.variants.index');
 
-Route::post('/shopify/variants', 'CP\VariantsController@store')
+Route::post('/shopify/variants', [VariantsController::class, 'store'])
     ->name('shopify.variants.store');
 
-Route::patch('/shopify/variants/{id}', 'CP\VariantsController@update')
+Route::patch('/shopify/variants/{id}', [VariantsController::class, 'update'])
     ->name('shopify.variants.edit');
 
 
