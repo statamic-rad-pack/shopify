e0754b856878f01682ff202c6fb6d2cf1c42fff5
Ryan Mitchell <ryan@thoughtcollective.com>
2023-09-18 08:04:36 +0100

    Setup test suite and add test coverage (#170)

--- src/Http/Controllers/Webhooks/WebhooksController.php
+++ src/Http/Controllers/Webhooks/WebhooksController.php
@@ -11,6 +11,10 @@ class WebhooksController extends CpController
      */
     protected function verify($data, $hmac_header): bool
     {
+        if (config('shopify.ignore_webhook_integrity_check', false)) {
+            return true;
+        }
+
         $calculated_hmac = base64_encode(hash_hmac('sha256', $data, config('shopify.webhook_secret'), true));
 
         return hash_equals($hmac_header, $calculated_hmac);
