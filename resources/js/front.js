851bd25479add268af6665bc170ccf093dbfcda9
Ryan Mitchell <ryan@thoughtcollective.com>
2023-09-19 09:50:31 +0100

    They arent classes, so dont use `new`.
    Webpack must be more forgiving than vite

--- resources/js/front.js
+++ resources/js/front.js
@@ -2,7 +2,7 @@ import checkout from './shopify/checkout'
 import cart, { setCartCount } from './shopify/cart'
 import productForm from './shopify/products'
 
-new checkout()
-new setCartCount()
-new productForm()
-new cart()
+checkout()
+setCartCount()
+productForm()
+cart()
