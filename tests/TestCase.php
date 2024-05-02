f111567acee2cf31ee9084e951fc659a64ee0184
Ryan Mitchell <ryan@thoughtcollective.com>
2024-05-02 09:05:14 +0100

    Support Statamic 5 (#208)

--- tests/TestCase.php
+++ tests/TestCase.php
@@ -8,66 +8,29 @@ use Statamic\Extend\Manifest;
 use Statamic\Facades;
 use Statamic\Providers\StatamicServiceProvider;
 use Statamic\Statamic;
+use Statamic\Testing\AddonTestCase;
+use Statamic\Testing\Concerns\PreventsSavingStacheItemsToDisk;
 use StatamicRadPack\Shopify\ServiceProvider;
 
-class TestCase extends OrchestraTestCase
+class TestCase extends AddonTestCase
 {
-    use AdditionalAssertions, PreventSavingStacheItemsToDisk;
+    use AdditionalAssertions, PreventsSavingStacheItemsToDisk;
 
-    public function setup(): void
-    {
-        parent::setup();
-        $this->preventSavingStacheItemsToDisk();
-    }
+    protected string $addonServiceProvider = ServiceProvider::class;
 
-    public function tearDown(): void
-    {
-        $this->deleteFakeStacheDirectory();
-
-        parent::tearDown();
-    }
-
-    protected function getPackageProviders($app)
-    {
-        return [StatamicServiceProvider::class, ServiceProvider::class];
-    }
-
-    protected function getPackageAliases($app)
-    {
-        return [
-            'Statamic' => Statamic::class,
-        ];
-    }
-
-    protected function getEnvironmentSetUp($app)
-    {
-        parent::getEnvironmentSetUp($app);
-
-        $app->make(Manifest::class)->manifest = [
-            'statamic-rad-pack/shopify' => [
-                'id' => 'statamic-rad-pack/shopify',
-                'namespace' => 'StatamicRadPack\\Shopify',
-            ],
-        ];
-
-        config(['statamic.users.repository' => 'file']);
-    }
+    protected $shouldFakeVersion = true;
 
     protected function resolveApplicationConfiguration($app)
     {
         parent::resolveApplicationConfiguration($app);
-
-        $configs = ['assets', 'cp', 'forms', 'routes', 'static_caching', 'sites', 'stache', 'system', 'users'];
-
-        foreach ($configs as $config) {
-            $app['config']->set("statamic.$config", require __DIR__."/../vendor/statamic/cms/config/{$config}.php");
-        }
 
         // Setting the user repository to the default flat file system
         $app['config']->set('statamic.users.repository', 'file');
 
         // Assume the pro edition within tests
         $app['config']->set('statamic.editions.pro', true);
+
+        $app['config']->set('statamic.system.multisite', true);
 
         Statamic::booted(function () {
             $blueprintContents = Facades\YAML::parse(file_get_contents(__DIR__.'/../resources/blueprints/collections/products/product.yaml'));
