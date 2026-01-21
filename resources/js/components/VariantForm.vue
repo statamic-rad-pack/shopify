356fc6c7d100d84e88738876f8dad2cd02333b73
Duncan McClean <duncan@duncanmcclean.com>
2026-01-21 12:59:08 +0000

    don't need the padding anymore either. that's handled by statamic now.

--- resources/js/components/VariantForm.vue
+++ resources/js/components/VariantForm.vue
@@ -1,18 +1,15 @@
  <template>
     <ui-stack :open="true" :size="narrow ? 'narrow' : null" @update:open="close">
-        <div class="h-full overflow-auto p-6">
-            <ui-publish-form
-                name="variant-publish-form"
-                :title="title"
-                :submit-url="action"
-                :submit-method="method"
-                :blueprint="blueprint"
-                :initial-meta="meta"
-                :initial-values="values"
-                @saved="saved"
-            >
-            </ui-publish-form>
-        </div>
+        <ui-publish-form
+            name="variant-publish-form"
+            :title="title"
+            :submit-url="action"
+            :submit-method="method"
+            :blueprint="blueprint"
+            :initial-meta="meta"
+            :initial-values="values"
+            @saved="saved"
+        ></ui-publish-form>
     </ui-stack>
 </template>
 
