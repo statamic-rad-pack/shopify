31113654c9e70d3f0bd1e07e6c541ceef76ef681
Ryan Mitchell <ryan@thoughtcollective.com>
2026-02-05 18:13:02 +0000

    make scopes selectable

--- src/Commands/ShopifyInstall.php
+++ src/Commands/ShopifyInstall.php
@@ -14,6 +14,90 @@ class ShopifyInstall extends Command
 
     protected $description = 'Install and authenticate with Shopify';
 
+    protected $availableScopes = [
+        'read_all_orders' => 'Read all orders',
+        'read_assigned_fulfillment_orders' => 'Read assigned fulfillment orders',
+        'write_assigned_fulfillment_orders' => 'Write assigned fulfillment orders',
+        'read_merchant_managed_fulfillment_orders' => 'Read merchant managed fulfillment orders',
+        'write_merchant_managed_fulfillment_orders' => 'Write merchant managed fulfillment orders',
+        'read_third_party_fulfillment_orders' => 'Read third party fulfillment orders',
+        'write_third_party_fulfillment_orders' => 'Write third party fulfillment orders',
+        'read_cart_transforms' => 'Read cart transforms',
+        'write_cart_transforms' => 'Write cart transforms',
+        'read_checkout_branding_settings' => 'Read checkout branding settings',
+        'write_checkout_branding_settings' => 'Write checkout branding settings',
+        'read_content' => 'Read content',
+        'write_content' => 'Write content',
+        'read_customer_events' => 'Read customer events',
+        'write_pixels' => 'Write pixels',
+        'read_customer_merge' => 'Read customer merge',
+        'write_customer_merge' => 'Write customer merge',
+        'read_customer_payment_methods' => 'Read customer payment methods',
+        'read_customers' => 'Read customers',
+        'write_customers' => 'Write customers',
+        'read_discounts' => 'Read discounts',
+        'write_discounts' => 'Write discounts',
+        'read_draft_orders' => 'Read draft orders',
+        'write_draft_orders' => 'Write draft orders',
+        'read_files' => 'Read files',
+        'write_files' => 'Write files',
+        'read_fulfillments' => 'Read fulfillments',
+        'write_fulfillments' => 'Write fulfillments',
+        'read_gift_cards' => 'Read gift cards',
+        'write_gift_cards' => 'Write gift cards',
+        'read_inventory' => 'Read inventory',
+        'write_inventory' => 'Write inventory',
+        'read_legal_policies' => 'Read legal policies',
+        'read_locations' => 'Read locations',
+        'read_marketing_events' => 'Read marketing events',
+        'write_marketing_events' => 'Write marketing events',
+        'read_merchant_approval_signals' => 'Read merchant approval signals',
+        'read_metaobject_definitions' => 'Read metaobject definitions',
+        'write_metaobject_definitions' => 'Write metaobject definitions',
+        'read_metaobjects' => 'Read metaobjects',
+        'write_metaobjects' => 'Write metaobjects',
+        'read_online_store_pages' => 'Read online store pages',
+        'write_online_store_pages' => 'Write online store pages',
+        'read_orders' => 'Read orders',
+        'write_orders' => 'Write orders',
+        'read_payment_terms' => 'Read payment terms',
+        'write_payment_terms' => 'Write payment terms',
+        'read_price_rules' => 'Read price rules',
+        'write_price_rules' => 'Write price rules',
+        'read_product_listings' => 'Read product listings',
+        'read_products' => 'Read products',
+        'write_products' => 'Write products',
+        'read_publications' => 'Read publications',
+        'write_publications' => 'Write publications',
+        'read_reports' => 'Read reports',
+        'write_reports' => 'Write reports',
+        'read_return_policies' => 'Read return policies',
+        'write_return_policies' => 'Write return policies',
+        'read_returns' => 'Read returns',
+        'write_returns' => 'Write returns',
+        'read_script_tags' => 'Read script tags',
+        'write_script_tags' => 'Write script tags',
+        'read_shipping' => 'Read shipping',
+        'write_shipping' => 'Write shipping',
+        'read_shopify_payments_disputes' => 'Read Shopify payments disputes',
+        'read_shopify_payments_payouts' => 'Read Shopify payments payouts',
+        'read_themes' => 'Read themes',
+        'write_themes' => 'Write themes',
+        'read_translations' => 'Read translations',
+        'write_translations' => 'Write translations',
+    ];
+
+    protected $defaultScopes = [
+        'write_customers',
+        'read_inventory',
+        'read_metaobjects',
+        'read_orders',
+        'read_product_listings',
+        'read_products',
+        'read_publications',
+        'read_translations',
+    ];
+
     public function handle()
     {
         $this->info('================================================================');
@@ -29,11 +113,14 @@ class ShopifyInstall extends Command
             return 1;
         }
 
+        // Select scopes
+        $selectedScopes = $this->selectScopes();
+        $scope = implode(',', $selectedScopes);
+
         // Request authorization
         $shopifyUrl = config('shopify.url');
         $redirectUrl = config('app.url') . '/admin/shopify/callback';
         $state = uniqid();
-        $scope = 'write_customers,read_inventory,read_metaobjects,read_orders,read_product_listings,read_products,read_publications,read_translations';
 
         $this->info('Requesting authorization...');
 
@@ -86,6 +173,26 @@ class ShopifyInstall extends Command
         $this->info('The admin token has been written to config/shopify.php');
 
         return 0;
+    }
+
+    protected function selectScopes()
+    {
+        $this->info('Select the API scopes you need (press space to toggle, enter to confirm):');
+
+        $choices = [];
+        foreach ($this->availableScopes as $scope => $description) {
+            $choices[] = $scope;
+        }
+
+        $selected = $this->choice(
+            'Which scopes do you need?',
+            $choices,
+            implode(',', array_keys(array_flip($this->defaultScopes))),
+            null,
+            true
+        );
+
+        return is_array($selected) ? $selected : [$selected];
     }
 
     protected function updateConfigFile($token)
