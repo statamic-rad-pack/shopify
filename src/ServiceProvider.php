ead0a8db1992d342b1183dadb41f0003a8004815
Ryan Mitchell <ryan@thoughtcollective.com>
2026-03-11 07:39:34 +0100

    Remove old cp js reference (#328)

--- src/ServiceProvider.php
+++ src/ServiceProvider.php
@@ -45,10 +45,6 @@ class ServiceProvider extends AddonServiceProvider
         Scopes\VariantIsOnSale::class,
     ];
 
-    protected $scripts = [
-        __DIR__.'/../dist/js/statamic-shopify-cp.js',
-    ];
-
     protected $tags = [
         Tags\Shopify::class,
     ];
