bea8f34c1fdba6bc8ab26d2ddb3ad0c6ad9bad3f
Ryan Mitchell <ryan@thoughtcollective.com>
2026-08-18 10:34:09 +0100

    Move settings page to inertia (#352)

--- resources/js/cp.js
+++ resources/js/cp.js
@@ -1,15 +1,10 @@
 import Variants from './fieldtypes/Variants.vue'
 import DisabledText from './fieldtypes/DisabledText.vue'
-import ImportProductButton from './components/ImportProductButton.vue'
-import ImportButton from './components/ImportButton.vue'
-import WebhookStatus from './components/WebhookStatus.vue'
+import Dashboard from './pages/Dashboard.vue'
 
 Statamic.booting(() => {
   Statamic.$components.register('variants-fieldtype', Variants)
   Statamic.$components.register('disabled_text-fieldtype', DisabledText)
 
-  // Dashboard Stuff
-  Statamic.$components.register('shopify-import-product-button', ImportProductButton)
-  Statamic.$components.register('shopify-import-button', ImportButton)
-  Statamic.$components.register('shopify-webhook-status', WebhookStatus)
+  Statamic.$inertia.register('shopify::Dashboard', Dashboard)
 });
