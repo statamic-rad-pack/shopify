5076278e973851dbbc5e480049f1de38c5f0da9d
Laurens Kuiper <104916503+lwekuiper@users.noreply.github.com>
2026-09-22 15:19:04 +0200

    Create the storefront client on first use (#373)
    Co-authored-by: Ryan Mitchell <ryan@thoughtcollective.com>

--- resources/js/shopify/client.js
+++ resources/js/shopify/client.js
@@ -1,13 +1,26 @@
 import { createStorefrontApiClient } from '@shopify/storefront-api-client';
 
+let instance = null;
+
 /**
  * Set up a new version of the Shopify Buy this uses the
  * token values set by {{ shopify:tokens }} in your template.
+ *
+ * The client is created on first use so that importing this module does not
+ * throw on pages that do not output {{ shopify:tokens }}.
  */
-const client = createStorefrontApiClient({
-    storeDomain: window.shopifyConfig.url,
-    apiVersion: window.shopifyConfig.apiVersion ?? '2024-07',
-    publicAccessToken: window.shopifyConfig.token,
-});
+const client = {
+    request(...args) {
+        if (! instance) {
+            instance = createStorefrontApiClient({
+                storeDomain: window.shopifyConfig.url,
+                apiVersion: window.shopifyConfig.apiVersion ?? '2024-07',
+                publicAccessToken: window.shopifyConfig.token,
+            });
+        }
+
+        return instance.request(...args);
+    },
+};
 
 export default client;
